135ebd0610562a58b7a706a87c2ef364b7e53f1d
Joel <134548469+Joel-Burget@users.noreply.github.com>
2025-05-01 13:30:56 -0600

    Created start of the form, created table and dummy data for the expsenses on the table

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import { useState } from "react";
 import "./App.css";
+import "./expense-tracker/components/Form/Form";
+import Form from "./expense-tracker/components/Form/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList/ExpenseList";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
 
-  return <></>;
+  return (
+    <>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
+      <Form />
+    </>
+  );
 }
 
 export default App;
---
+++ src/expense-tracker/components/Form/Form.tsx
@@ -0,0 +1,59 @@
+import { FormEvent, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  description: z.string().min(3),
+  amount: z.number(),
+  category: z.string(),
+});
+
+type FormData = z.infer<typeof schema>;
+
+const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  const onSubmit = (data: FieldValues) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3">
+        <label htmlFor="description" className="form-label">
+          Description
+        </label>
+        <input
+          {...register("description")}
+          id="description"
+          type="text"
+          className="form-control"
+        />
+        {errors.description && (
+          <p>Description is required and must be longer than 3 characters.</p>
+        )}
+      </div>
+      <div className="mb-3">
+        <label htmlFor="amount" className="form-label">
+          Amount
+        </label>
+        <input
+          {...register("amount", { valueAsNumber: true })}
+          id="amount"
+          type="number"
+          className="form-control"
+        />
+        {errors.amount && <p>Must enter an amount</p>}
+      </div>
+      <div className="mb-3"></div>
+      <button disabled={!isValid} className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
