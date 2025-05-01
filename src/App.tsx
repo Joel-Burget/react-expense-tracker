fb21e22da7cdd8f4d9e058cddaa10941985fe57b
Joel <134548469+Joel-Burget@users.noreply.github.com>
2025-05-01 14:25:43 -0600

    Finshed form logic, added dynamic categories for form and table, added form validation

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,41 @@ import "./App.css";
 import "./expense-tracker/components/Form/Form";
 import Form from "./expense-tracker/components/Form/Form";
 import ExpenseList from "./expense-tracker/components/ExpenseList/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter/ExpenseFilter";
+import categories from "./expense-tracker/categories";
 
 function App() {
+  const [seletedCategory, setSelectedCategory] = useState("");
   const [expenses, setExpenses] = useState([
     { id: 1, description: "aaa", amount: 10, category: "Utilities" },
     { id: 2, description: "bbb", amount: 10, category: "Utilities" },
-    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Groceries" },
     { id: 4, description: "ddd", amount: 10, category: "Utilities" },
   ]);
 
+  const visbileExpenses = seletedCategory
+    ? expenses.filter((e) => e.category === seletedCategory)
+    : expenses;
+
   return (
     <>
+      <div className="mb-5">
+        <Form
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+
       <ExpenseList
-        expenses={expenses}
+        expenses={visbileExpenses}
         onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       />
-      <Form />
     </>
   );
 }
--- src/expense-tracker/components/Form/Form.tsx
+++ src/expense-tracker/components/Form/Form.tsx
@@ -2,26 +2,35 @@ import { FormEvent, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import categories from "../../categories";
+
+interface Props {
+  onSubmit: (data: ExpenseFormData) => void;
+}
 
 const schema = z.object({
   description: z.string().min(3),
-  amount: z.number(),
-  category: z.string(),
+  amount: z.number().min(0.01),
+  category: z.enum(categories),
 });
 
-type FormData = z.infer<typeof schema>;
+type ExpenseFormData = z.infer<typeof schema>;
 
-const Form = () => {
+const Form = ({ onSubmit }: Props) => {
   const {
     register,
     handleSubmit,
-    formState: { errors, isValid },
-  } = useForm<FormData>({ resolver: zodResolver(schema) });
-
-  const onSubmit = (data: FieldValues) => console.log(data);
+    reset,
+    formState: { errors },
+  } = useForm<ExpenseFormData>({ resolver: zodResolver(schema) });
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form
+      onSubmit={handleSubmit((data) => {
+        onSubmit(data);
+        reset();
+      })}
+    >
       <div className="mb-3">
         <label htmlFor="description" className="form-label">
           Description
@@ -33,7 +42,9 @@ const Form = () => {
           className="form-control"
         />
         {errors.description && (
-          <p>Description is required and must be longer than 3 characters.</p>
+          <p className="text-danger">
+            Description is required and must be longer than 3 characters.
+          </p>
         )}
       </div>
       <div className="mb-3">
@@ -46,12 +57,28 @@ const Form = () => {
           type="number"
           className="form-control"
         />
-        {errors.amount && <p>Must enter an amount</p>}
+        {errors.amount && <p className="text-danger">Must enter an amount</p>}
       </div>
-      <div className="mb-3"></div>
-      <button disabled={!isValid} className="btn btn-primary">
-        Submit
-      </button>
+      <div className="mb-3">
+        <label htmlFor="category">Category</label>
+        <select
+          {...register("category")}
+          name="category"
+          id=""
+          className="form-select"
+        >
+          <option value=""></option>
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+        {errors.category && (
+          <p className="text-danger">Please select a category.</p>
+        )}
+      </div>
+      <button className="btn btn-primary">Submit</button>
     </form>
   );
 };
